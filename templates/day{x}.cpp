83dea847a5d97790e38bc2696e84f8897df6b1bc
jacobjwood <jacobjwood@gmail.com>
2024-12-07 19:37:24 +0000

    Update template

--- templates/day{x}.cpp
+++ templates/day{x}.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include "helpers.hpp"
+#include <vector>
+#include <unordered_map>
 
 int main() {
     std::string input = helpers::read_input("day<DAY>/input.txt");
